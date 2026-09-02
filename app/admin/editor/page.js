b5c4442a2c08e6e9610c77608201e0691bade922
PRONO2828 <166699922+PRONO2828@users.noreply.github.com>
2026-09-01 22:47:27 -0700

    cursor image-upload button + editable dashboard message (app/admin/editor/page.js)

--- app/admin/editor/page.js
+++ app/admin/editor/page.js
@@ -1,6 +1,5 @@
 import { redirect } from "next/navigation";
 import { getAdminSession } from "../../../lib/auth";
-import { getHomeHtml } from "../../../lib/content";
 import SiteEditor from "../../../components/SiteEditor";
 
 export const dynamic = "force-dynamic";
@@ -8,6 +7,5 @@ export const dynamic = "force-dynamic";
 export default async function EditorPage() {
   const admin = await getAdminSession();
   if (!admin) redirect("/admin/login");
-  const html = await getHomeHtml();
-  return <SiteEditor initialHtml={html} />;
+  return <SiteEditor />;
 }
